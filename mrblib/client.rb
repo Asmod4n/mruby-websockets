4a570f572bf378d4f5c2165194987e2dbb42512e
Asmod4n <Asmod4n@users.noreply.github.com>
2015-07-19 15:46:17 +0200

    catch EAGAIN in mruby-wslay

--- mrblib/client.rb
+++ mrblib/client.rb
@@ -11,7 +11,6 @@ module WebSocket
       @socket.write("GET #{path} HTTP/1.1\r\nHost: #{host}:#{port}\r\nConnection: Upgrade\r\nUpgrade: WebSocket\r\nSec-WebSocket-Version: 13\r\nSec-WebSocket-Key: #{key}\r\n\r\n")
       buf = @socket.recv 16384
       phr = Phr.new
-      ret = nil
       loop do
         ret = phr.parse_response(buf)
         case ret
@@ -31,27 +30,9 @@ module WebSocket
       end
       @socket._setnonblock(true)
       @callbacks = Wslay::Event::Callbacks.new
-      @callbacks.recv_callback do |len|
-        begin
-          @socket.recv len, Socket::MSG_DONTWAIT
-        rescue Errno::EAGAIN, Errno::EWOULDBLOCK
-          nil
-        rescue => e
-          raise e
-        end
-      end
-      @callbacks.send_callback do |buf|
-        begin
-          @socket.send buf, Socket::MSG_DONTWAIT
-        rescue Errno::EAGAIN, Errno::EWOULDBLOCK
-          nil
-        rescue => e
-          raise e
-        end
-      end
-      @callbacks.genmask_callback do |buf, len|
-        RandomBytes.buf buf, len
-      end
+      @callbacks.recv_callback {|buf, len| @socket.recv len, Socket::MSG_DONTWAIT}
+      @callbacks.send_callback {|buf| @socket.send buf, Socket::MSG_DONTWAIT}
+      @callbacks.genmask_callback {|buf, len| RandomBytes.buf buf, len}
       @msgs = []
       @callbacks.on_msg_recv_callback {|msg| @msgs << msg}
       @client = Wslay::Event::Context::Client.new @callbacks
