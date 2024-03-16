74e5177e394ab4e13b8045db12769ad9e323589c
Asmod4n <791770+Asmod4n@users.noreply.github.com>
2024-03-16 18:59:12 +0100

    updates for latest mruby-wslay and mruby-tls

--- README.md
+++ README.md
@@ -10,20 +10,11 @@ Then add
 ```
 to your build_config.rb
 
+
 Example
-=======
-```ruby
-client = WebSocket::Client.new(:ws, "echo.websocket.org", 80, "/")
-client.send "hallo"
-client.recv
-client.close
-
-````
-
-wss Example
 ===========
 ```ruby
-client = WebSocket::Client.new(:wss, "echo.websocket.org", 443, '/', ciphers: 'TLSv1.2') # needed for amazon web services
+client = WebSocket::Client.new("echo.websocket.org", 443, '/', ciphers: 'TLSv1.2') # needed for amazon web services
 client.send "hallo"
 client.recv
 client.close
--- mrblib/client.rb
+++ mrblib/client.rb
@@ -3,12 +3,15 @@ module WebSocket
     B64.encode(Sysrandom.buf(16)).chomp!
   end
 
-  class WsConnection
+  class Connection
     def initialize(host, port, path, *args)
       @host = host
       @port = port
       @path = path
-      @socket = TCPSocket.new host, port
+      @tcp_socket = TCPSocket.new host, port
+      @socket = Tls::Client.new(*args)
+      @socket.connect_socket @tcp_socket.fileno, host
+      setup()
     end
 
     def recv(timeout)
@@ -22,7 +25,11 @@ module WebSocket
     ensure
       @socket_pi.events = Poll::Out
       while @client.want_write?
-        @client.send if @poll.wait(timeout)
+        if @poll.wait(timeout)
+          @client.send
+        else
+          break
+        end
       end
     end
 
@@ -34,7 +41,11 @@ module WebSocket
       end
       @socket_pi.events = Poll::Out
       while @client.want_write?
-        @client.send if @poll.wait(timeout)
+        if @poll.wait(timeout)
+          @client.send
+        else
+          break
+        end
       end
       self
     end
@@ -52,120 +63,72 @@ module WebSocket
         if @poll.wait(timeout)
           @client.send if @socket_pi.writable?
           @client.recv if @socket_pi.readable?
+        else
+          break
         end
       end
       @msgs.dup
     ensure
       @msgs.clear
+      @tcp_socket._setnonblock(false)
       @socket.close
     end
 
+    private
     def setup
       http_handshake
       make_nonblock
-      setup_ws
       setup_poller
+      setup_ws
     rescue => e
+      @tcp_socket._setnonblock(false)
       @socket.close
       raise e
     end
 
-    private
     def http_handshake
       key = WebSocket.create_key
       @socket.write("GET #{@path} HTTP/1.1\r\nHost: #{@host}:#{@port}\r\nConnection: Upgrade\r\nUpgrade: websocket\r\nSec-WebSocket-Version: 13\r\nSec-WebSocket-Key: #{key}\r\n\r\n")
-      buf = @socket.recv(16384)
+      buf = @socket.read
       phr = Phr.new
       while true
         case phr.parse_response(buf)
         when Fixnum
           break
         when :incomplete
-          buf << @socket.recv(16384)
+          buf << @socket.read
         when :parser_error
           raise Error, "HTTP Parser error"
         end
       end
+      puts phr.headers.to_h
       unless WebSocket.create_accept(key).securecmp(phr.headers.to_h.fetch('sec-websocket-accept'))
         raise Error, "Handshake failure"
       end
     end
 
     def make_nonblock
-      @socket._setnonblock(true)
-    end
-
-    def setup_ws
-      @callbacks = Wslay::Event::Callbacks.new
-      @callbacks.recv_callback {|buf, len| @socket.recv len}
-      @callbacks.send_callback {|buf| @socket.write buf}
-      @msgs = []
-      @callbacks.on_msg_recv_callback {|msg| @msgs << msg}
-      @client = Wslay::Event::Context::Client.new @callbacks
-    end
-
-    def setup_poller
-      @poll = Poll.new
-      @socket_pi = @poll.add(@socket)
-    end
-  end
-
-  class WssConnection < WsConnection
-    def initialize(host, port, path, *args)
-      @host = host
-      @port = port
-      @path = path
-      @tcp_socket = TCPSocket.new host, port
-      @socket = Tls::Client.new(*args)
-      @socket.connect_socket @tcp_socket.fileno, host
-    end
-
-    private
-    def make_nonblock
       @tcp_socket._setnonblock(true)
     end
-
-    def setup_ws
-      @callbacks = Wslay::Event::Callbacks.new
-      @callbacks.recv_callback do |buf, len|
-        ret = -1
-        begin
-          ret = @socket.recv len
-        rescue Tls::WantPollin, Tls::WantPollout
-          raise Errno::EWOULDBLOCK
-        end
-        ret
-      end
-      @callbacks.send_callback do |buf|
-        ret = -1
-        begin
-          ret = @socket.write buf
-        rescue Tls::WantPollin, Tls::WantPollout
-          raise Errno::EWOULDBLOCK
-        end
-        ret
-      end
-      @msgs = []
-      @callbacks.on_msg_recv_callback {|msg| @msgs << msg}
-      @client = Wslay::Event::Context::Client.new @callbacks
-    end
-
 
     def setup_poller
       @poll = Poll.new
       @socket_pi = @poll.add(@tcp_socket)
     end
+
+    def setup_ws
+      @callbacks = Wslay::Event::Callbacks.new
+      @callbacks.recv_callback {|buf, len| @socket.read_nonblock len}
+      @callbacks.send_callback {|buf| @socket.write_nonblock buf}
+      @msgs = []
+      @callbacks.on_msg_recv_callback {|msg| @msgs << msg}
+      @client = Wslay::Event::Context::Client.new @callbacks
+    end
   end
 
   class Client
-    def initialize(proto, host, port, path, *args)
-      case proto
-      when :ws
-        @connection = WsConnection.new(host, port, path, *args)
-      when :wss
-        @connection = WssConnection.new(host, port, path, *args)
-      end
-      @connection.setup
+    def initialize(host, port, path, *args)
+      @connection = Connection.new(host, port, path, *args)
     end
 
     def recv(timeout = -1)
